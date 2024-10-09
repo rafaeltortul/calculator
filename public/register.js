f251493ff7553975375b973b8ee4db05d4e61af4
rafaeltortul <rafael.tortul@hotmail.com>
2024-10-09 16:13:30 -0400

    Update register.js

--- public/register.js
+++ public/register.js
@@ -32,11 +32,56 @@ function generateCode() {
         if (result.success) {
             console.log("Código enviado com sucesso");
             document.getElementById('codeSection').style.display = 'block';
+            document.getElementById('codeSection').scrollIntoView({ behavior: 'smooth' });
+            alert("Código de verificação enviado com sucesso para o seu celular.");
         } else {
             console.error("Erro ao enviar o código:", result.message);
+            alert("Erro ao enviar o código. Por favor, tente novamente.");
         }
     })
     .catch(error => {
         console.error("Erro de rede ou no servidor:", error);
+        alert("Ocorreu um erro ao gerar o código. Por favor, tente novamente.");
+    });
+}
+
+function verifyCode() {
+    const phone = document.getElementById('phone').value;
+    const verificationCode = document.getElementById('verificationCode').value;
+
+    console.log("Verificando o código de verificação...");
+
+    const data = {
+        phone,
+        code: verificationCode
+    };
+
+    fetch('/verify-code', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        console.log("Resposta recebida:", response);
+        if (!response.ok) {
+            throw new Error(`Erro no servidor: ${response.statusText}`);
+        }
+        return response.json();
+    })
+    .then(result => {
+        if (result.success) {
+            console.log("Código verificado com sucesso");
+            alert("Código autenticado com sucesso!");
+            window.location.href = "/index.html"; // Redirecionar para a página index.html
+        } else {
+            console.error("Código inválido:", result.message);
+            alert("Código inválido. Por favor, tente novamente.");
+        }
+    })
+    .catch(error => {
+        console.error("Erro de rede ou no servidor:", error);
+        alert("Erro ao verificar o código. Por favor, tente novamente.");
     });
 }
