1793931523f22cf96379f38267e32f2dcafbd815
rafaeltortul <rafael.tortul@hotmail.com>
2024-10-16 10:27:06 -0400

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ let verificationCodes = {};
 
 // Rota para redirecionar para a página de registro
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'register.html'));
+    res.redirect(path.join(__dirname, 'public', 'register.html'));
 });
 
 // Rota para enviar o código de verificação
@@ -33,8 +33,6 @@ app.post('/register', async (req, res) => {
     const verificationCode = Math.floor(1000 + Math.random() * 9000);
     const formattedPhone = `+55${phone.replace(/[^0-9]/g, '')}`;
 
-    console.log("Tentando enviar código de verificação para:", formattedPhone);
-    
     try {
         // Enviar SMS via Twilio
         const message = await client.messages.create({
@@ -42,15 +40,10 @@ app.post('/register', async (req, res) => {
             from: process.env.TWILIO_PHONE_NUMBER,
             to: formattedPhone
         });
-        console.log("Mensagem Twilio enviada com sucesso, SID:", message.sid);
+        verificationCodes[phone] = verificationCode;
+        console.log(`Código de verificação armazenado para o telefone: ${phone} ${verificationCode}`);
 
-        // Armazenar o código de verificação
-        verificationCodes[formattedPhone] = verificationCode;
-        console.log("Código de verificação armazenado para o telefone:", formattedPhone, verificationCode);
-        
-        res.status(200).json({ success: true, message: "Código de verificação enviado com sucesso!" });
-
-        // Enviar e-mail de notificação para o administrador
+        // Tentar enviar o e-mail de notificação
         const transporter = nodemailer.createTransport({
             service: 'hotmail',
             auth: {
@@ -67,33 +60,36 @@ app.post('/register', async (req, res) => {
         };
 
         await transporter.sendMail(mailOptions);
-        console.log("E-mail de notificação enviado.");
+        console.log('E-mail enviado com sucesso.');
+
+        // Enviar a resposta de sucesso ao cliente
+        res.status(200).json({ success: true, message: "Código de verificação enviado com sucesso." });
+
     } catch (error) {
         console.error('Erro ao enviar SMS ou e-mail:', error.message);
-        res.status(500).json({ success: false, message: "Erro ao enviar código de verificação.", error: error.message });
+
+        // Verificar se a mensagem SMS foi enviada com sucesso
+        if (!res.headersSent) {
+            res.status(500).json({ success: false, message: "Erro ao enviar o código de verificação." });
+        }
     }
 });
 
 // Rota para verificar o código de verificação
 app.post('/verify-code', (req, res) => {
     const { phone, code } = req.body;
-    const formattedPhone = `+55${phone.replace(/[^0-9]/g, '')}`;
-    
-    console.log("Verificando o código para o telefone:", formattedPhone, "com código:", code);
 
-    if (verificationCodes[formattedPhone] && verificationCodes[formattedPhone] === parseInt(code)) {
-        delete verificationCodes[formattedPhone]; // Limpar o código após a verificação
-        console.log("Código verificado com sucesso!");
-        res.status(200).json({ success: true, message: "Código verificado com sucesso!" });
+    if (verificationCodes[phone] && verificationCodes[phone] === parseInt(code)) {
+        delete verificationCodes[phone]; // Limpar o código após a verificação
+        res.status(200).json({ success: true, message: "Código verificado com sucesso." });
     } else {
-        console.log("Código inválido.");
         res.status(400).json({ success: false, message: "Código inválido." });
     }
 });
 
 // Redirecionar qualquer outra rota não especificada para a página de registro
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'register.html'));
+    res.redirect('/');
 });
 
 // Definindo a porta para o servidor escutar
